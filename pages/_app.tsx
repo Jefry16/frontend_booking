2e5326971aa7fb8cf8a8bcfe696f07e46feba338
Jefry Cayo <jefrycayo16@gmail.com>
2022-11-15 12:59:17 +0100

    added react query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,19 @@
 import type { AppProps } from "next/app";
 import Layout from "../components/layout";
-import '../styles/less/antd.less'
+import "../styles/less/antd.less";
 import "../styles/global.scss";
+import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <QueryClientProvider client={queryClient}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
